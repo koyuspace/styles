fb3c97ca7b467bcb29ce6275471b46a3fbbf55a1
koyu <me@koyu.space>
2020-10-29 08:03:52 +0100

    Fix instance ticker being fucky with sassc

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 command -v sassc >/dev/null 2>&1 || { echo >&2 "I require sassc but it's not installed.  Aborting."; exit 1; }
 wget -O instanceticker.scss https://miy.pw/css/2.css
-sassc plugins.scss > output.css
+sassc plugins.scss > output1.css
+cat instanceticker.scss output1.css > output.css
+rm output1.css
