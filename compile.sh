8da98cc3b23d2eb70b8061f2e46a6a88bfbdcb6c
koyu <me@koyu.space>
2020-10-29 07:55:58 +0100

    Update instanceticker

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 command -v sassc >/dev/null 2>&1 || { echo >&2 "I require sassc but it's not installed.  Aborting."; exit 1; }
+wget -O instanceticker.scss https://miy.pw/css/2.css
 sassc plugins.scss > output.css
