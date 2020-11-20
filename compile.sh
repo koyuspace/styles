e5cd0e505404a6db65d8cdafc262217c4ad4a305
koyu <me@koyu.space>
2020-11-20 12:40:00 +0100

    Fix logo and compilation process

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+rm -f output.css
 command -v sassc >/dev/null 2>&1 || { echo >&2 "I require sassc but it's not installed.  Aborting."; exit 1; }
 wget -O instanceticker.scss https://miy.pw/css/2.css
 sassc plugins.scss > output1.css
