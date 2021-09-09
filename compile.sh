fbff16d500743472bf16022ec7d9ca84410af3a9
koyu <me@koyu.space>
2021-09-09 22:50:18 +0200

    Fix instanceticker

--- compile.sh
+++ compile.sh
@@ -1,6 +1,3 @@
 rm -f output.css
 command -v sassc >/dev/null 2>&1 || { echo >&2 "I require sassc but it's not installed.  Aborting."; exit 1; }
-wget -O instanceticker.scss https://miy.pw/css/2.css
-sassc plugins.scss > output1.css
-cat instanceticker.scss output1.css > output.css
-rm output1.css
+sassc plugins.scss > output.css
