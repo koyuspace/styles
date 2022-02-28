a6367db089d212fd012d95ee2fffab7da2f5b1ba
koyu <me@koyu.space>
2022-02-28 17:59:51 +0100

    Add date to CSS file to fix caching issues

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 rm -f output.css
 command -v sassc >/dev/null 2>&1 || { echo >&2 "I require sassc but it's not installed.  Aborting."; exit 1; }
 sassc plugins.scss > output.css
+echo -e "/* Compiled $(LANG=C date -u) */" >> output.css
